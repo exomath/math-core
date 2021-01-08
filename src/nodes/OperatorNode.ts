e66bfe07ade4848925da8735d9fb032a3335ff9c
Jake McKown <jbmckown@gmail.com>
2021-01-07 21:55:53 -0700

    Rename OperationNode to OperatorNode

--- src/nodes/OperatorNode.ts
+++ src/nodes/OperatorNode.ts
@@ -1,26 +1,26 @@
-import { Node } from './Node';
+import { Node } from './';
 
 const validOperatorTypes = ['string'];
 
-export class OperationNode implements Node {
-  public readonly class = 'OperationNode';
+export class OperatorNode implements Node {
+  public readonly class = 'OperatorNode';
 
   constructor(public readonly operator: string, public readonly operands: Node[]) {
     if (!validOperatorTypes.includes(typeof operator)) {
-      throw new TypeError(`OperationNode: "operator" cannot be of type "${typeof operator}", must be "${validOperatorTypes.join('" | "')}"`);
+      throw new TypeError(`OperatorNode: "operator" cannot be of type "${typeof operator}", must be "${validOperatorTypes.join('" | "')}"`);
     }
 
     if (!(Array.isArray(operands) && operands.every(operand => operand.hasOwnProperty('class') && operand.class.endsWith('Node')))) {
-      throw new TypeError('OperationNode: "operands" must be an Array with items of class "Node"');
+      throw new TypeError('OperatorNode: "operands" must be an Array with items of class "Node"');
     }
 
     Object.defineProperty(this, 'arity', {
-      get() { return this.operands.length; },
+      get: () => { return this.operands.length; },
       enumerable: true
     });
   }
 
   public static new(operator: string, operands: Node[]) {
-    return Object.freeze(new OperationNode(operator, operands));
+    return Object.freeze(new OperatorNode(operator, operands));
   }
 }
