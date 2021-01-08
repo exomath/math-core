388d6fc2f2a14726e8dc5a21132fa8566ab3fb9a
Jake McKown <jbmckown@gmail.com>
2021-01-08 16:10:39 -0700

    Update IdentifierNode.ts

--- src/nodes/IdentifierNode.ts
+++ src/nodes/IdentifierNode.ts
@@ -5,17 +5,17 @@ const TYPE = 'IdentifierNode';
 
 export class IdentifierNode extends Node {
   public constructor(
-    public readonly name: string
+    public readonly identifier: string
   ) {
     super(TYPE);
 
-    if (!isString(name)) {
-      throw new TypeError(`${TYPE}: "name" must be of type string`);
+    if (!isString(identifier)) {
+      throw new TypeError(`${TYPE}: "identifier" must be of type string`);
     }
   }
 
-  public static new(name: string) {
-    return Object.freeze(new IdentifierNode(name));
+  public static new(identifier: string) {
+    return Object.freeze(new IdentifierNode(identifier));
   }
 
   public static isIdentifierNode(value: any): boolean {
