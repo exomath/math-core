3e5d8e96f68a4ce36cf5aac549a559602f2237a4
Jake McKown <jbmckown@gmail.com>
2021-01-08 16:10:46 -0700

    Create RecordNode.ts

---
+++ src/nodes/RecordNode.ts
@@ -0,0 +1,29 @@
+import { Node, IdentifierNode } from './';
+import { hasType, isNull } from '../utils';
+
+const TYPE = 'RecordNode';
+
+export class RecordNode extends Node {
+  public constructor(
+    public readonly map: Map<IdentifierNode, Node>,
+    public readonly primary: IdentifierNode | null = null
+  ) {
+    super(TYPE);
+
+    if (!(map instanceof Map)) {
+      throw new TypeError(`${TYPE}: "map" must be of type Map<IdentifierNode, *Node>`);
+    }
+
+    if (!(IdentifierNode.isIdentifierNode(primary) || isNull(primary))) {
+      throw new TypeError(`${TYPE}: "primary" must be of type IdentifierNode | null`);
+    }
+  }
+
+  public static new(map: Map<IdentifierNode, Node>, primary: IdentifierNode | null = null) {
+    return Object.freeze(new RecordNode(map, primary));
+  }
+
+  public static isRecordNode(value: any): boolean {
+    return hasType(value, TYPE);
+  }
+}
