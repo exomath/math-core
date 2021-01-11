da94b9069f8d72b0e5f1c58461fd8cfe5ec6d212
Jake McKown <jbmckown@gmail.com>
2021-01-11 13:07:25 -0700

    Update repl.js

--- bin/repl.js
+++ bin/repl.js
@@ -1,17 +1,17 @@
-'use strict'
+'use strict';
 
-const mathCore = require('../index')
+const mathCore = require('../index');
 
-const repl = require('repl').start()
+const repl = require('repl').start();
 
-Object.assign(repl.context, mathCore)
+Object.assign(repl.context, mathCore);
 
 Object.defineProperty(repl.context, 'exit', {
   enumerable: true,
   get: () => process.exit(0)
-})
+});
 
 Object.defineProperty(repl.context, 'quit', {
   enumerable: true,
   get: () => repl.context.exit
-})
+});
