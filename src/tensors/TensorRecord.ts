db2ecfceaccfe2b494c2921de9386f19ab3d5271
Jake McKown <jbmckown@gmail.com>
2021-01-14 15:26:13 -0700

    Update TensorRecord.ts

--- src/tensors/TensorRecord.ts
+++ src/tensors/TensorRecord.ts
@@ -1,9 +1,11 @@
+export type TensorView = Int32Array | Float64Array;
+
 export class TensorRecord {
   private constructor(
     public offset: number,
     public length: number,
     public dtype: string,
-    public view: Float64Array
+    public view: TensorView
   ) {}
 
   public static new(offset: number, length: number, dtype: string, view: Float64Array) {
