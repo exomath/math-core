72042e317bf82f2751115f72cb8a63929c6c208a
Jake McKown <jbmckown@gmail.com>
2021-01-14 11:00:37 -0700

    Add TypedArrays

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -37,3 +37,66 @@ export function isNumberArray(value: any): boolean {
 export function isString(value: any): boolean {
   return typeof value === 'string' || value instanceof String;
 }
+
+// TypedArrays
+
+export function isInt8Array(value: any): boolean {
+  return value instanceof Int8Array;
+}
+
+export function isUint8Array(value: any): boolean {
+  return value instanceof Uint8Array;
+}
+
+export function isUint8ClampedArray(value: any): boolean {
+  return value instanceof Uint8ClampedArray;
+}
+
+export function isInt16Array(value: any): boolean {
+  return value instanceof Int16Array;
+}
+
+export function isUint16Array(value: any): boolean {
+  return value instanceof Uint16Array;
+}
+
+export function isInt32Array(value: any): boolean {
+  return value instanceof Int32Array;
+}
+
+export function isUint32Array(value: any): boolean {
+  return value instanceof Uint32Array;
+}
+
+// Not currently supporting BigInt64Array and BigUint64Array
+
+export function isFloat32Array(value: any): boolean {
+  return value instanceof Float32Array;
+}
+
+export function isFloat64Array(value: any): boolean {
+  return value instanceof Float64Array;
+}
+
+export function isIntArray(value: any): boolean {
+  return isInt8Array(value)
+      || isInt16Array(value)
+      || isInt32Array(value);
+}
+
+export function isUintArray(value: any): boolean {
+  return isUint8Array(value)
+      || isUint16Array(value)
+      || isUint32Array(value);
+}
+
+export function isFloatArray(value: any): boolean {
+  return isFloat32Array(value)
+      || isFloat64Array(value);
+}
+
+export function isTypedArray(value: any): boolean {
+  return isIntArray(value)
+      || isUintArray(value)
+      || isFloatArray(value);
+}
