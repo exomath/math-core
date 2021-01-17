07fc8b794f678075ddbd3bdeb2a32710eb184b4e
Jake McKown <jbmckown@gmail.com>
2021-01-17 15:15:35 -0700

    Remove utils

--- src/utils/has.ts
+++
@@ -1,9 +0,0 @@
-import { isObject } from '.';
-
-export function hasOwnProperty(value: any, property: string): boolean {
-  return isObject(value) && value.hasOwnProperty(property);
-}
-
-export function hasType(value: any, type: string): boolean {
-  return hasOwnProperty(value, 'type') && value.type === type;
-}
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './assert';
-export * from './encoding';
-export * from './has';
-export * from './is';
